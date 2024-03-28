2970036fc87b1347e35235f49883cb1694946d18
mahedi205339 <mahedi205339gmaol.com>
2024-03-28 21:59:32 +0600

    initialized full setup

---
+++ src/routes/routes.js
@@ -0,0 +1,22 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../page/Home/Home";
+import Layout from "../Layout/Layout";
+
+
+
+const router = createBrowserRouter([
+
+    {
+        path: '/',
+        element: <Layout />,
+        // errorElement: ,
+        children: [
+            {
+                path: '/',
+                element: <Home />
+            }
+        ]
+    }
+])
+
+export default router;
